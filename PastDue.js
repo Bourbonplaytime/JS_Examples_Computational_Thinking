0e662902e5c255efaff0deca2e6c84ab3af098a0
Bourbs <Bourbonplaytime@gmail.com>
2019-11-07 15:57:55 -0800

    Update

--- PastDue.js
+++ PastDue.js
@@ -1,11 +1,12 @@
-amount = prompt("Enter the amount owed.");
-pastDue = prompt("Enter the days past due");
+input = [{amount: 100, pastDue: 30}, {amount: 100, pastDue: 10}, {amount:10, pastDue: 10}, 
+        {amount:50, pastDue: 25}, {amount: 200, pastDue: 20}]
 penalty = 15;
 
-
-if(amount > 10){
-    if(pastDue > 15) {
-        console.log(parseInt(amount));
+for(i = 0; i < input.length; i++) {
+    if(input[i].amount > 10){
+        if(input[i].pastDue > 15) {
+            input[i].amount = parseInt(input[i].amount) + penalty;
+            console.log(input[i].amount);
+        }
     }
-    //console.log(amount);
 }
