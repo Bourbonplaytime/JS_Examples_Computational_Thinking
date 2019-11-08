6025a10417d432f74daa7dcb211b0a1fd8bf6fb7
Bourbs <Bourbonplaytime@gmail.com>
2019-11-08 14:54:33 -0800

    Added functionality to exit loop early, compute prices

--- item_finder.js
+++ item_finder.js
@@ -1,15 +1,18 @@
 let validItems = [106, 108, 307, 405, 457, 688];
+let prices = [12, 15, 18, 21, 24, 27];
+let amountDue = 0;
 itemInCart = [];
 
 let itemNum = prompt("Enter item number you would like to order or 'quit' to end.");
 
 while(itemNum != "quit") {
     let foundIt = false;
-    for(i = 0; i < validItems.length; i++) {
+    for(i = 0; i < validItems.length && foundIt != true; i++) {
         if(itemNum == validItems[i]) {
             alert("Item available!");
             foundIt = true;
             itemInCart.push(itemNum);
+            amountDue += prices[i];
         }
     }
     if(foundIt == false) {
@@ -19,3 +22,4 @@ while(itemNum != "quit") {
 }
 
 console.log(itemInCart);
+console.log("The total cost of this order is $" + amountDue);
